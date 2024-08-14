dfc765e193284df837e2def881b15ca88b7e5565
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-08-14 14:05:08 -0700

    adding default ggplot theme

---
+++ analysis/figures/default-ggplot-theme.R
@@ -0,0 +1,7 @@
+library('ggplot2') # for fancy figures
+
+theme_set(theme_bw() +
+            theme(panel.grid = element_blank(),
+                  text = element_text(face = 'bold')))
+
+PAL <- c(control = '#CC3311', ovariectomy = '#009988')
