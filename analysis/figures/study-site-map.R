38a4a3f4338783e6447558dfbac6dbe9f82749b1
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-10-04 11:51:00 -0700

    added insets in site map

--- analysis/figures/study-site-map.R
+++ analysis/figures/study-site-map.R
@@ -1,7 +1,9 @@
 library('dplyr')   # for data wrangling
 library('sf')      # for spatial data
+library('spData')  # for US shapefile
 library('ggplot2') # for fancy figures
 library('ggmap')   # for basemaps
+library('cowplot') # for fancy multi-panel figures
 source('analysis/figures/default-ggplot-theme.R')
 
 # deer telemetry data
@@ -43,12 +45,13 @@ area <- bind_rows(stations, tels) %>%
   st_as_sf() %>%
   st_buffer(1e3)
 
-se <- area %>%
+se_map <- area %>%
   st_bbox() %>%
   `names<-`(c('left', 'bottom', 'right', 'top')) %>%
   get_stadiamap(maptype = 'stamen_terrain', bbox = ., zoom = 14)
 
-ggmap(se) +
+p_a <-
+  ggmap(se_map) +
   geom_path(aes(X, Y, group = animal), bind_cols(tels, st_coordinates(tels)),
             inherit.aes = FALSE, alpha = 0.5, linewidth = 0.1) +
   geom_sf(data = tels, inherit.aes = FALSE, alpha = 0.5, size = 0.2) +
@@ -58,5 +61,29 @@ ggmap(se) +
   geom_sf(data = stations, inherit.aes = FALSE, size = 1.75, color = 'white') +
   labs(x = 'Longitude', y = 'Latitude')
 
+se <- stations %>%
+  select(! name) %>%
+  st_union() %>%
+  st_centroid() %>%
+  st_as_sf()
+
+us_states <- st_transform(us_states, 'EPSG:32617')
+
+ohio <- filter(us_states, NAME == 'Ohio')
+
+p_b <- ggplot() +
+  geom_sf(data = ohio, fill = 'black') +
+  geom_sf(data = se, color = 'darkorange', size = 2) +
+  theme_map()
+
+p_c <- ggplot() +
+  geom_sf(data = us_states, fill = 'black') +
+  geom_sf(data = ohio, fill = 'darkorange') +
+  theme_map()
+
+plot_grid(p_a,
+          plot_grid(p_b, p_c, labels = c('B', 'C'), nrow = 1),
+          ncol = 1, rel_heights = c(6, 1))
+
 ggsave('figures/south-euclid-map.png',
-       width = 7, height = 14, units = 'in', dpi = 600, bg = 'white')
+       width = 7, height = 14 * (7/6), units = 'in', dpi = 600, bg = 'white')
